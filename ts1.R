0681f1de3e27d78758b9855405b60a55a7c15a7c
Rosyid <rds081295@gmail.com>
2024-02-07 14:55:39 +0700

    add library dplyr

--- ts1.R
+++ ts1.R
@@ -1,15 +1,26 @@
+#import packages ggplot2 & dplyr
 library(ggplot2)
+library(dplyr)
+
+#get data from csv
 data <- read.csv("C:/Users/Nusatrip/OneDrive/Documents/IPB/Semester 2/Data mining/crop/Crops_AllData_Normalized.csv")
+
+#summary data
 summary(data)
+
 #get country liberia
 data_liberia <- filter(data, Area=="Liberia")
 
-#convert string to numuric
+#mapping variabel
 mapping <- c("Area harvested" = "0", "Yield" = "1", "Production" = "2")
+
+#convert string to numuric
 data_liberia$Element <- mapping[as.character(data_liberia$Element)]
+
 #applyTo
 data_liberia$Element <- as.numeric(data_liberia$Element)
 
+#create histogram
 ggplot(data_liberia, aes(x = Element)) +
   geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
   coord_cartesian(ylim = c(1900, 2050)) + 
